9bc53c87ef277a32651bf1bbbf7d7d2c3b5e28ca
Brian <bernaldev3@gmail.com>
2022-01-28 00:38:19 -0500

    swagger setup

--- docs/basic-info.js
+++ docs/basic-info.js
@@ -1,30 +1,62 @@
 module.exports = {
     "openapi": "3.0.0",
     "info": {
-        "title": "Sample API",
-        "description": "Optional multiline or single-line description in [CommonMark](http://commonmark.org/help/) or HTML.",
-        "version": "0.1.9"
+        "title": "Blog API",
+        "description": "API for interacting with a blog backend written using the express.js framework",
+        "version": "0.1.0"
     },
     "paths": {
-        "/blog": {
-            "get": {
-                "summary": "Returns a list of public blog posts.",
-                "description":""
+        "/auth/register": {
+            "post": {
+                "summary": "Registers a user using the information request body."
+                
+            }
+        },
+        "/auth/login": {
+            "post": {
+                "summary": "Logins with the specified username and password in the request body.",
             }
         },
         "/blog/username": {
             "get": {
-                "summary": "Returns a list of public blog posts.",
-                "parameters" : {
-                    name: "username", 
-                    in: "path", 
-                    schema: {
-                      $ref: "#/schemas/auth", 
-                    },
-                    required: true, 
-                    description: "a blog username", 
-                  },
+                "summary": "Returns all of the blogs for the username."
+            }
+        },
+        "/blog/{username}": {
+            "post": {
+                "summary": "Creates a post from the request body JSON.",
+                "parameters" : [{
+                    "name": "username", 
+                    "in": "path", 
+                    "required": "true", 
+                    "description": "a username", 
+                  }]
+            }
+        },
+        "/blog/id": {
+            "put": {
+                "summary": "updates the post at the specified id with the request body",
+                "parameters" : [{
+                    "name": "id", 
+                    "in": "path", 
+                    
+                    "required": "true", 
+                    "description": "a blog id", 
+                  }]
+            }
+        },
+        "/blog/{id}": {
+            "delete": {
+                "summary": "deletes the post with the specified id",
+                "parameters" : [{
+                    "name": "id", 
+                    "in": "path", 
+                    
+                    "required": "true", 
+                    "description": "a blog id", 
+                  }]
             }
         }
+
     }
 }
