97b68001d898f3b085e20c763c1e699d5950583d
Brian <bernaldev3@gmail.com>
2022-01-28 11:57:23 -0500

    updated swagger

--- docs/basic-info.js
+++ docs/basic-info.js
@@ -1,29 +1,61 @@
 module.exports = {
     "openapi": "3.0.0",
+
     "info": {
         "title": "Blog API",
-        "description": "API for interacting with a blog backend written using the express.js framework",
-        "version": "0.1.0A"
+        "description": "API for registering and logging in users. API for creating, reading, updating, and deleting blog posts.",
+        "version": "1.0.0"
     },
     "paths": {
         "/auth/register": {
             "post": {
-                "summary": "Registers a user using the information in the request body."
-            }
+                "summary": "Registers a user using the information in the request body.",
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/user", // todo input data model
+                            },
+                        },
+                    },
+                }
+            },
         },
         "/auth/login": {
             "post": {
                 "summary": "Logins with the specified username and password in the request body.",
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "properties": {
+                                    "username": {
+                                        "type": "string"
+                                    },
+                                    "password": {
+                                        "type": "string"
+                                    },
+                                },
+                            },
+                        },
+                    },
+                }
             }
         },
         "/blog": {
             "get": {
-                "summary": "Returns all public blog posts."
+                "summary": "Returns all public blog posts."    
             }
         },
         "/blog/{username}": {
             "get": {
-                "summary": "Returns all of the blogs for the username."
+                "summary": "Returns all of the blogs for the username.",
+                "parameters": [{
+                    "name": "username",
+                    "in": "path",
+                    "required": "true",
+                    "description": "a username",
+                }]
             },
             "post": {
                 "summary": "Creates a post using the information in the request body.",
@@ -32,7 +64,16 @@ module.exports = {
                     "in": "path",
                     "required": "true",
                     "description": "a username",
-                }]
+                }],
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/user", // todo input data model
+                            },
+                        },
+                    },
+                }
             }
         },
         "/blog/{id}": {
@@ -43,7 +84,20 @@ module.exports = {
                     "in": "path",
                     "required": "true",
                     "description": "a blog id",
-                }]
+                }],
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "properties": {
+                                    "property": {
+                                        "type": "string"
+                                    }
+                                },
+                            },
+                        },
+                    },
+                }
             },
             "delete": {
                 "summary": "Deletes the post with the specified id",
@@ -54,6 +108,50 @@ module.exports = {
                     "description": "a blog id",
                 }]
             }
+        }
+    },
+    "components": {
+        "schemas": {
+            "user": {
+                "type": "object", // data type
+                "properties": {
+                    "username": {
+                        "type": "string"
+                    },
+                    "email": {
+                        "type": "string"
+                    },
+                    "birthday": {
+                        "type": "string"
+                    },
+                    "age": {
+                        "type": "string"
+                    },
+                    "password": {
+                        "type": "string"
+                    }
+                }
+            },
+            "blog": {
+                "type": "object", // data type
+                "properties": {
+                    "created_by": {
+                        "type": "string"
+                    },
+                    "created_at": {
+                        "type": "date"
+                    },
+                    "blog_title": {
+                        "type": "string"
+                    },
+                    "blog_content": {
+                        "type": "string"
+                    },
+                    "private": {
+                        "type": "boolean"
+                    }
+                }
+            },
         }
     }
 }
